d37fc0a9f98d15cf34ca29d93c43d3353786333b
James Vannicola <jim@pop-os.home>
2024-10-06 08:53:30 -0400

    basic functionality working

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,4 +1,4 @@
-/* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
+const products = [];
 
 /* Create 3 or more product objects using object literal notation 
    Each product should include five properties
@@ -9,6 +9,32 @@
    - image: picture of product (url string)
 */
 
+products.push(
+	{
+	name: "cherry",
+	price: 2.00,
+	quantity: 0,
+	productId: 1,
+	image: "/images/cherry.jpg"
+	},
+
+	{
+	name: "orange",
+	price: 2.00,
+	quantity: 0,
+	productId: 2,
+	image: "/images/orange.jpg"
+	},
+
+	{
+	name: "strawberry",
+	price: 2.00,
+	quantity: 0,
+	productId: 3,
+	image: "/images/strawberry.jpg"
+	},
+);
+
 /* Images provided in /images folder. All images from Unsplash.com
    - cherry.jpg by Mae Mu
    - orange.jpg by Mae Mu
@@ -17,16 +43,33 @@
 
 /* Declare an empty array named cart to hold the items in the cart */
 
-/* Create a function named addProductToCart that takes in the product productId as an argument
+const cart = [];
+
+/* Create a function named addPreductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
   - addProductToCart should then increase the product's quantity
   - if the product is not already in the cart, add it to the cart
 */
 
+function addProductToCart (productId) {
+	if(cart.find(fruit => fruit.productId === productId)) {
+		//  set the quantity ++
+		increaseQuantity(productId);
+	} else {
+		// Add the product to the cart and set increase quantity
+		cart.push(products.find(product => product.productId === productId));
+		increaseQuantity(productId);
+	}	
+};
+
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
+
+function increaseQuantity (productId) {
+	cart.find(fruit => fruit.productId === productId).quantity++;
+}
 
 /* Create a function named decreaseQuantity that takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
@@ -34,11 +77,25 @@
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
 
+function decreaseQuantity (productId) {
+	const item = cart.find(fruit => fruit.productId === productId);
+	item.quantity--;
+	if(item.quantity === 0) {
+		removeProductFromCart(productId);
+	}
+}
+
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
   - removeProductFromCart should remove the product from the cart
 */
+
+function removeProductFromCart (productId) {
+	const index = cart.findIndex(fruit => fruit.productId === productId);
+	cart[index].quantity = 0;
+	cart.splice(index, 1);
+}
 
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total cost of all products
@@ -46,7 +103,19 @@
   Hint: price and quantity can be used to determine total cost
 */
 
+function cartTotal () {
+	let total = 0;
+	cart.forEach(fruit => {
+		total += fruit.price * fruit.quantity;
+	});
+	return total;
+}
+
 /* Create a function called emptyCart that empties the products from the cart */
+
+function emptyCart () {
+	cart.splice(0);
+}
 
 /* Create a function named pay that takes in an amount as an argument
   - amount is the money paid by customer
@@ -54,6 +123,10 @@
   - pay will return a positive number if money should be returned to customer
   Hint: cartTotal function gives us cost of all the products in the cart  
 */
+
+function  pay (amount) {
+	return amount - cartTotal();
+}
 
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
 
